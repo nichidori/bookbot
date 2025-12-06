b37a3e9cc5f0316d64baf0b529a58dc6172566e1
Arrafly Dori <arraflydori@gmail.com>
2025-12-06 21:58:29 +0700

    ch3 l2

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ def get_book_text(file_path):
     return contents
 
 def main():
-    frankenstein_content = get_book_text("books/frankenstein.txt")
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_book = sys.argv[1]
+    frankenstein_content = get_book_text(path_to_book)
     num_words = get_num_words(frankenstein_content)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_book}")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
