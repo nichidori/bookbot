611efd5264ebdbf6e3c3bc6fbc1db1ee7e7d88a0
Arrafly Dori <arraflydori@gmail.com>
2025-12-06 21:29:39 +0700

    ch2 l6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 def get_book_text(file_path):
     contents = None
@@ -10,5 +10,7 @@ def main():
     frankenstein_content = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(frankenstein_content)
     print(f"Found {num_words} total words")
+    num_chars = get_num_chars(frankenstein_content)
+    print(num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
-def get_num_words(contents):
-    return len(contents.split())
+def get_num_words(text):
+    return len(text.split())
+
+def get_num_chars(text):
+    num_chars = {}
+    for char in text:
+        lowcase_char = char.lower()
+        if lowcase_char in num_chars:
+            num_chars[lowcase_char] += 1
+        else:
+            num_chars[lowcase_char] = 1
+    return num_chars
