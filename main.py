658aebdbd0c046ac140d32d8ffac5f3881b72ff8
Arrafly Dori <arraflydori@gmail.com>
2025-12-06 21:54:35 +0700

    ch3 l1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, get_sorted_dict
+import sys
 
 def get_book_text(file_path):
     contents = None
@@ -9,8 +10,21 @@ def get_book_text(file_path):
 def main():
     frankenstein_content = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(frankenstein_content)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
     num_chars = get_num_chars(frankenstein_content)
-    print(num_chars)
+    sorted_num_chars = get_sorted_dict(num_chars)
+
+    for dict in sorted_num_chars:
+        if not dict["char"].isalpha():
+            continue
+        print(f"{dict["char"]}: {dict["num"]}")
+    
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,16 @@ def get_num_chars(text):
         else:
             num_chars[lowcase_char] = 1
     return num_chars
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_dict(dict):
+    list = []
+    for key in dict:
+        inner_dict = {}
+        inner_dict["char"] = key
+        inner_dict["num"] = dict[key]
+        list.append(inner_dict)
+    list.sort(key=sort_on, reverse=True)
+    return list
