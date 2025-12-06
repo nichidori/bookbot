37eed5e165d0e1539d64e3a4320d5bc86749e45d
Arrafly Dori <arraflydori@gmail.com>
2025-12-06 21:23:50 +0700

    ch2 l5

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_num_words
+
 def get_book_text(file_path):
     contents = None
     with open(file_path) as f:
@@ -6,7 +8,7 @@ def get_book_text(file_path):
 
 def main():
     frankenstein_content = get_book_text("books/frankenstein.txt")
-    num_words = len(frankenstein_content.split())
+    num_words = get_num_words(frankenstein_content)
     print(f"Found {num_words} total words")
 
 main()
---
+++ stats.py
@@ -0,0 +1,2 @@
+def get_num_words(contents):
+    return len(contents.split())
